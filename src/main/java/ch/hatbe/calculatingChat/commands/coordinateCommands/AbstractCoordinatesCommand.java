5d3faf1606fcfa44cca89cfe8a61837b9e7c47ca
HATBE2113 <admin@aarongensetter.ch>
2025-01-07 23:12:14 +0100

    copy to clipboard action

--- src/main/java/ch/hatbe/calculatingChat/commands/coordinateCommands/AbstractCoordinatesCommand.java
+++ src/main/java/ch/hatbe/calculatingChat/commands/coordinateCommands/AbstractCoordinatesCommand.java
@@ -1,10 +1,17 @@
 package ch.hatbe.calculatingChat.commands.coordinateCommands;
 
 import ch.hatbe.calculatingChat.coordinates.Coordinates2D;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+// TODO: if just enter command with no args, it should use players location && add tab complete for player location
+// TODO: handle y coordinate
 
 public abstract class AbstractCoordinatesCommand implements CommandExecutor {
     @Override
@@ -25,11 +32,26 @@ public abstract class AbstractCoordinatesCommand implements CommandExecutor {
         }
 
         Coordinates2D coordinates2D = calculateCoordinates(x, z);
+        String dimensionName = this.getDimensionName();
 
-        sender.sendMessage(String.format("%s coordinates: x: %d, z: %d", this.getDimensionName(), coordinates2D.x(), coordinates2D.z()));
+        // TODO: create object && class to handle this shit
+        String clipboardText = String.format("%d %d", coordinates2D.x(), coordinates2D.z());
+        String displayText = String.format("%s coordinates: x: %d, z: %d", dimensionName, coordinates2D.x(), coordinates2D.z());
+
+        if (sender instanceof Player player) {
+            Component message = Component.text(displayText)
+                    .hoverEvent(HoverEvent.showText(Component.text("Click to copy coordinates!")))
+                    .clickEvent(ClickEvent.copyToClipboard(clipboardText));
+
+            player.sendMessage(message);
+        } else {
+            sender.sendMessage(displayText);
+        }
 
         return true;
     }
+
+
 
     private String sendUsageMessage(String cmd) {
         return String.format("usage: /%s <x> <z>", cmd);
