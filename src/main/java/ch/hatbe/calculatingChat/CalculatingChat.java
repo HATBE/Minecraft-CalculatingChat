968a34e7b2c3dbca1899d9f17cece088b25651e0
HATBE2113 <admin@aarongensetter.ch>
2025-01-08 10:04:46 +0100

    handle y coords

--- src/main/java/ch/hatbe/calculatingChat/CalculatingChat.java
+++ src/main/java/ch/hatbe/calculatingChat/CalculatingChat.java
@@ -2,6 +2,7 @@ package ch.hatbe.calculatingChat;
 
 import ch.hatbe.calculatingChat.commands.coordinateCommands.NetherCoordinatesCommand;
 import ch.hatbe.calculatingChat.commands.coordinateCommands.OverworldCoordinatesCommand;
+import ch.hatbe.calculatingChat.events.OnPlayerChat;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -19,6 +20,7 @@ public final class CalculatingChat extends JavaPlugin {
         this.getLoggerInstance().info("Plugin enabled");
 
         this.registerCommands();
+        this.registerEvents();
     }
 
     @Override
@@ -29,6 +31,10 @@ public final class CalculatingChat extends JavaPlugin {
     private void registerCommands() {
         this.getCommand("nethercoordinates").setExecutor(new NetherCoordinatesCommand());
         this.getCommand("overworldcoordinates").setExecutor(new OverworldCoordinatesCommand());
+    }
+
+    private void registerEvents() {
+        this.getServer().getPluginManager().registerEvents(new OnPlayerChat(), this);
     }
 
     private void setInstance(CalculatingChat instance) {
--- src/main/java/ch/hatbe/calculatingChat/commands/coordinateCommands/AbstractCoordinatesCommand.java
+++ src/main/java/ch/hatbe/calculatingChat/commands/coordinateCommands/AbstractCoordinatesCommand.java
@@ -11,17 +11,16 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 // TODO: add tab complete for player location
-// TODO: handle y coordinate
-
 public abstract class AbstractCoordinatesCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
-        int x, z;
+        Integer x, y, z;
 
         switch(args.length) {
             case 0:
                 if (sender instanceof Player player) {
                     x = player.getLocation().getBlockX();
+                    y = player.getLocation().getBlockY();
                     z = player.getLocation().getBlockZ();
                 } else {
                     sender.sendMessage(this.sendUsageMessage(label));
@@ -31,7 +30,18 @@ public abstract class AbstractCoordinatesCommand implements CommandExecutor {
             case 2:
                 try {
                     x = Integer.parseInt(args[0]);
+                    y = null;
                     z = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    sender.sendMessage("Error: Coordinates must be valid integers.");
+                    return false;
+                }
+                break;
+            case 3:
+                try {
+                    x = Integer.parseInt(args[0]);
+                    y = Integer.parseInt(args[1]);
+                    z = Integer.parseInt(args[2]);
                 } catch (NumberFormatException e) {
                     sender.sendMessage("Error: Coordinates must be valid integers.");
                     return false;
@@ -46,8 +56,15 @@ public abstract class AbstractCoordinatesCommand implements CommandExecutor {
         String dimensionName = this.getDimensionName();
 
         // TODO: create object && class to handle this shit
-        String clipboardText = String.format("%d %d", coordinates.x(), coordinates.z());
-        String displayText = String.format("%s coordinates: x: %d, z: %d", dimensionName, coordinates.x(), coordinates.z());
+        String clipboardText =
+                y == null ?
+                String.format("%d %d", coordinates.x(), coordinates.z()) :
+                String.format("%d %d %d", coordinates.x(), y, coordinates.z());
+
+        String displayText =
+                y == null ?
+                String.format("%s coordinates: x: %d, z: %d", dimensionName, coordinates.x(), coordinates.z()) :
+                String.format("%s coordinates: x: %d, y: %d, z: %d", dimensionName, coordinates.x(), y, coordinates.z());
 
         if (sender instanceof Player player) {
             Component message = Component.text(displayText)
@@ -63,7 +80,7 @@ public abstract class AbstractCoordinatesCommand implements CommandExecutor {
     }
 
     private String sendUsageMessage(String cmd) {
-        return String.format("usage: /%s <x> <z>", cmd);
+        return String.format("usage: /%s <x> <z> or /%s <x> <y> <z>", cmd, cmd);
     }
 
     protected abstract Coordinates2D calculateCoordinates(int x, int z);
